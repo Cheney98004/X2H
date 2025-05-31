1adc11858ccff28f6c394c3e6f799b67c65fd5a5
Cheney98004 <123722523+Cheney98004@users.noreply.github.com>
2025-05-31 17:33:54 +0800

    test

--- src/elements/checkbox.js
+++ src/elements/checkbox.js
@@ -10,14 +10,26 @@ export default class Checkbox extends BaseElement {
         const checkbox = document.createElement("input");
         const label = document.createElement("label");
 
+        container.style.display = "flex";
+        container.style.alignItems = "center";
+        container.style.gap = "8px";
+
         checkbox.type = "checkbox";
         checkbox.checked = checked;
         checkbox.name = name;
         checkbox.id = attrs.id || name;
+        checkbox.style.width = "16px";
+        checkbox.style.height = "16px";
+        checkbox.style.margin = "0";
+        checkbox.style.cursor = "pointer";
 
         label.htmlFor = checkbox.id;
         label.textContent = attrs.text;
-
+        label.style.cursor = "pointer";
+        label.style.userSelect = "none";
+        label.style.color = attrs.color;
+        label.style.fontSize = `${attrs.fontSize}px`;
+        
         container.appendChild(checkbox);
         container.appendChild(label);
         
